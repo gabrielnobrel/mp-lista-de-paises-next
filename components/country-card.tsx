e0555a2de746f0d4e5fc83e17796803f7522eaa2
gabrielnobrel <gabriel_nobresantos@hotmail.com>
2026-04-13 14:20:56 -0300

    style(country-card): ajusta altura da flag de h-40 para h-20

--- components/country-card.tsx
+++ components/country-card.tsx
@@ -11,7 +11,7 @@ const CountryCard = ({ data }: { data: CountryListDTO[] }) => {
           key={country.name.common}
           className="rounded-xl border-2 bg-white p-2"
         >
-          <div className="relative h-40 w-full overflow-hidden rounded-xl p-2">
+          <div className="relative h-20 w-full overflow-hidden rounded-xl p-2">
             <Image
               src={country.flags.svg}
               alt={`Flag of ${country.name.common}`}
@@ -20,7 +20,7 @@ const CountryCard = ({ data }: { data: CountryListDTO[] }) => {
               loading="eager"
             />
           </div>
-          <h2 className="mt-2 text-center">{country.name.common ?? "-"}</h2>
+          <h1 className="mt-2 text-center">{country.name.common ?? "-"}</h1>
         </Link>
       ))}
     </div>
