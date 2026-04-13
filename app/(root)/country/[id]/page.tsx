448c406292175feb7bfafacf1c762749c3d7cf34
gabrielnobrel <gabriel_nobresantos@hotmail.com>
2026-04-13 18:49:27 -0300

    chore(country): adiciona metadata estático na página de detalhes

--- app/(root)/country/[id]/page.tsx
+++ app/(root)/country/[id]/page.tsx
@@ -2,12 +2,17 @@ import CountryDetails from "@/components/country-details";
 import { getCountryByName } from "@/data-access/get-countries";
 import { buildCountryDetails } from "@/lib/utils/country-details";
 
+export const metadata = {
+  title: "Detalhes do País",
+};
+
 const Country = async ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = await params;
 
   const country = await getCountryByName(id);
   const details = buildCountryDetails(country[0]);
 
+  console.log("country", country);
   return (
     <>
       <section className="container w-full rounded-xl">
